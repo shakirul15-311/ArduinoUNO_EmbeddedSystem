30d1016fc3dbfc8cf13e9e9a3e7b43f8818a2a69
Mohammad Shakirul Islam <shakirul15-311@diu.edu.bd>
2018-07-02 23:08:10 +0600

    lm

--- LM35_temp/lm_35.ino
+++ LM35_temp/lm_35.ino
@@ -1,6 +1,7 @@
 void setup()
 {
   pinMode(A1, INPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -9,6 +10,14 @@ void loop()
  
   int temp = analogRead(A1);
   float value = (temp *0.48);
-  
   Serial.println(value);
+  delay(1000);
+  
+  if(value>32){
+    digitalWrite(13, HIGH);
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+  
 }
